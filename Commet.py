2adf453829ddb524cbbcfec686d877846b117a30
COLLET Guillaume <guillaume.collet@irisa.fr>
2014-10-27 11:13:38 +0100

    added header for python scripting

--- Commet.py
+++ Commet.py
@@ -22,6 +22,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#!/usr/bin/env python
+
 import os
 import sys
 import errno
